d6cbeecdb8081d82196cb6a9d8f9b91f966d2294
Antonia K <antonia.korba@gmail.com>
2022-11-22 19:31:03 +0200

    Add files via upload

---
+++ LFTJ.cpp
@@ -0,0 +1,141 @@
+#include "LFTJS.h"
+
+#include <stdlib.h>
+
+using namespace types;
+
+Level::Level(std::vector<TrieIterator*> iters){
+    for (auto iter: iters) iters_.push_back(iter);
+}
+
+LFTJ::LFTJ(DataHandler<int> *dH, bool count) : dH_(dH), countTuples_(count) {
+#ifdef MEASURE_TIME
+    auto time1 = std::chrono::steady_clock::now();
+#endif
+
+    attrOrder_.reserve(dH_->attrs);
+    for (int i = 0; i < dH_->attrs; ++i) attrOrder_.push_back(i);
+
+    for (int table = 0; table < dH_->tables; ++table) {
+        if(dH_->database[table].size() == 0) {
+            isEmpty_ = true;
+            return;
+        }
+    }
+
+    for (int table = 0; table < dH_->tables; ++table) {
+        std::vector<int> order;
+        order.reserve(dH_->IDs[table].size());
+        for (auto attr: attrOrder_) {
+            auto it = find(dH_->IDs[table].begin(), dH_->IDs[table].end(), attr);
+            if (it != dH_->IDs[table].end()){
+                int index = it - dH_->IDs[table].begin();
+                order.push_back(index);
+            }
+        }
+        trieIterators_[table] = new TrieIterator(dH_->database[table], order);
+    }
+
+    levels_.reserve(dH_->attrs);
+    for (auto attr: attrOrder_){
+        std::vector<TrieIterator*> iters;
+        iters.reserve(dH_->ta[attr].size());
+        for(auto table: dH_->ta[attr]) iters.push_back(trieIterators_[table.tableID]);
+        levels_.push_back(new Level(iters));
+    }
+}
+
+long long LFTJ::Count() {
+    if (isEmpty_) return count_;
+    LFTJ::Open();
+    while(true) {
+        if(levels_[level_]->atEnd) {
+            if (level_ == 0) break;
+            else {
+                LFTJ::Up();
+                LFTJ::Next();
+            }
+        } else {
+            if (level_ == dH_->attrs - 1) LFTJ::Next();
+            else LFTJ::Open();
+        }
+    }
+    return count_;
+}
+
+void LFTJ::Init() {
+    if(AnyIterAtEnd()) {
+        levels_[level_]->atEnd = true;
+    } else {
+        levels_[level_]->atEnd = false;
+        std::sort(  levels_[level_]->iters_.begin(),
+            levels_[level_]->iters_.end(), [this](TrieIterator* it_a, TrieIterator* it_b) {
+                if (it_a->key() < it_b->key()) return true;
+                return false;
+        });
+        levels_[level_]->p = 0;
+        LFTJ::Search();
+    }
+}
+
+void LFTJ::Open() {
+    level_++;
+    for(auto iter: levels_[level_]->iters_) iter->open();
+    LFTJ::Init();
+}
+
+void LFTJ::Up() {
+    for(auto iter: levels_[level_]->iters_) iter->up();
+    level_--;
+}
+
+void LFTJ::Search() {
+    int k = levels_[level_]->iters_.size();
+    int x_ = levels_[level_]->iters_[utils::mod(levels_[level_]->p-1, k)]->key();
+    while(true) {
+        int x = levels_[level_]->iters_[levels_[level_]->p]->key();
+        if (x == x_) {
+            UpdateResult();
+            return;
+        } else {
+            levels_[level_]->iters_[levels_[level_]->p]->seek(x_);
+            if (levels_[level_]->iters_[levels_[level_]->p]->atEnd()) {
+                levels_[level_]->atEnd = true;
+                return;
+            } else {
+                x_ = levels_[level_]->iters_[levels_[level_]->p]->key();
+                levels_[level_]->p = utils::mod(levels_[level_]->p + 1, k);
+            }
+        }
+    }
+}
+
+void LFTJ::Next() {
+    levels_[level_]->iters_[levels_[level_]->p]->next();
+    if(levels_[level_]->iters_[levels_[level_]->p]->atEnd()) {
+        levels_[level_]->atEnd = true;
+    } else {
+        int k = levels_[level_]->iters_.size();
+        levels_[level_]->p = utils::mod(levels_[level_]->p + 1, k);
+        LFTJ::Search();
+    }
+}
+
+void LFTJ::UpdateResult() {
+    if(level_ == 0) levels_[level_]->count = 1;
+    if(level_ > 0) levels_[level_]->count = levels_[level_-1]->count;
+    for (auto iter: levels_[level_]->iters_) {
+        if(iter->isLeaf()) levels_[level_]->count *= iter->count();
+    }
+    if(level_ == dH_->attrs - 1) {
+        count_ += levels_[level_]->count;
+        // resultTable.push_back();
+    }
+}
+
+bool LFTJ::AnyIterAtEnd() {
+    for(auto iter: levels_[level_]->iters_) {
+        if (iter->atEnd()) return true;
+    }
+    return false;
+}
---
+++ LFTJ.h
@@ -0,0 +1,49 @@
+#ifndef LFTJ_H
+#define LFTJ_H
+
+#include <numeric>
+#include "TrieIterator.h"
+
+using namespace types;
+
+class Level {
+public:
+    bool atEnd;
+    int p;
+    int key;
+    long long count;
+    std::vector<TrieIterator*> iters_;
+
+public:
+    Level(std::vector<TrieIterator*> iters);
+};
+
+class LFTJ {
+public:
+    explicit LFTJ(DataHandler<int> *dH, bool count = false);
+    ~LFTJ() = default;
+    long long Count();
+
+private:
+    DataHandler<int> *dH_;
+    bool countTuples_;
+    Table<int> resultTable_;
+    std::unordered_map<int, TrieIterator*> trieIterators_;
+    std::vector<int> attrOrder_;
+    std::vector<Level*> levels_;
+    bool isEmpty_ = false;
+    int level_ = -1;
+    bool totalEnd_ = false;
+    long long count_ = 0;
+
+    void Open();
+    void Init();
+    void Search();
+    void Next();
+    void Up();
+    void UpdateResult();
+    bool AnyIterAtEnd();
+};
+
+
+#endif //LFTJ_H
